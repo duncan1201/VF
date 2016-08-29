4944b8e77966a8fd47e4e431b40c16c689e71f34
Dunqiang Liao <dunqiang.liao@gmail.com>
2016-08-30 07:15:31 +0800

    mac: add msa support

--- netbeans/gas/msa/src/com/gas/msa/actions/MSAAction.java
+++ netbeans/gas/msa/src/com/gas/msa/actions/MSAAction.java
@@ -41,10 +41,13 @@ import java.awt.Component;
 import java.awt.Frame;
 import java.awt.event.ActionEvent;
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import javax.swing.AbstractAction;
+import org.apache.commons.io.FileUtils;
 import org.openide.DialogDescriptor;
 import org.openide.DialogDisplayer;
+import org.openide.util.Exceptions;
 import org.openide.util.Lookup;
 import org.openide.util.lookup.ServiceProvider;
 import org.openide.windows.WindowManager;
@@ -93,21 +96,21 @@ public class MSAAction extends AbstractAction implements IMSAUIService {
             }
         } else {
             if (profiles.size() > 2) {
-                msg = "Please select no more than two alignments for profile alignment" ;                
+                msg = "Please select no more than two alignments for profile alignment";
                 ret = false;
             } else if (profiles.size() == 2) {
                 if (input.size() > 0) {
-                    msg = String.format("<html>Two aligments and %d sequence(s) selected. <br/><br/>Please unselect one alignment or the sequences for profile alignment</html>", input.size());                    
+                    msg = String.format("<html>Two aligments and %d sequence(s) selected. <br/><br/>Please unselect one alignment or the sequences for profile alignment</html>", input.size());
                     ret = false;
-                }else{
+                } else {
                     MSAList msaList = msaService.getFull(profiles);
                     ret = checkDuplicateSeqs(msaList, new AnnotatedSeqList(input));
                 }
             } else if (profiles.size() == 1) {
                 if (input.size() == 0) {
-                    msg = "<html>Only one alignment is selected. <br/><br/>Please select another alignment or sequences for profile alignment</html>";                    
+                    msg = "<html>Only one alignment is selected. <br/><br/>Please select another alignment or sequences for profile alignment</html>";
                     ret = false;
-                }else{
+                } else {
                     MSAList msaList = msaService.getFull(profiles);
                     ret = checkDuplicateSeqs(msaList, new AnnotatedSeqList(input));
                 }
@@ -159,7 +162,7 @@ public class MSAAction extends AbstractAction implements IMSAUIService {
         }
         clustalParam.getGeneralParam().setType(isNucleotide ? GeneralParam.TYPE.DNA : GeneralParam.TYPE.PROTEIN);
 
-        if(msaPanel.getVfMsaUI() != null){
+        if (msaPanel.getVfMsaUI() != null) {
             msaPanel.getVfMsaUI().setAminoAcids(!isNucleotide);
         }
         msaPanel.getClustalWUI().setMsaParam(clustalParam);
@@ -179,6 +182,7 @@ public class MSAAction extends AbstractAction implements IMSAUIService {
 
                 @Override
                 public void run(ProgressHandle handle) {
+                    checkExecutable(selected);
                     handle.progress("Preparing data...");
                     handle.setIndeterminate(true);
                     File inFile = null;
@@ -215,6 +219,7 @@ public class MSAAction extends AbstractAction implements IMSAUIService {
                         }
                     }
                     if (selected instanceof ClustalWUI) {
+
                         ClustalWUI c = (ClustalWUI) selected;
                         ClustalwParam clustalwParam = c.getClustalwParam();
 
@@ -251,14 +256,14 @@ public class MSAAction extends AbstractAction implements IMSAUIService {
                         msa = muscleService.align(muscleUIParams);
                         msaService.createConsensus(msa);
                         msaService.createQualityScores(msa);
-                    } else if(selected instanceof IVfMsaUI){
+                    } else if (selected instanceof IVfMsaUI) {
                         handle.progress("Running VectorFriends aligner...");
                         IVfMsaUI vfMsaParam = (IVfMsaUI) selected;
                         VfMsaParam param = vfMsaParam.getVfMsaParam();
                         msa = vfMsaService.getMSA(inputFull, param);
-                        if(msa.isDnaByGuess()){
+                        if (msa.isDnaByGuess()) {
                             msa.setType("DNA");
-                        }else{
+                        } else {
                             msa.setType("Protein");
                         }
                         msaService.createConsensus(msa);
@@ -300,6 +305,20 @@ public class MSAAction extends AbstractAction implements IMSAUIService {
         }
     }
 
+    private void checkExecutable(Component selected) {
+        if (selected instanceof ClustalWUI) {
+            if (!clustalService.isExecutablePresent()) {
+                File dir = clustalService.getExecutableDirectory();
+                try {
+                    FileUtils.copyFileToDirectory(new File("//Users//Dunqiang//Documents//clustalw2"), dir);
+                } catch (IOException ex) {
+                    Exceptions.printStackTrace(ex);
+                }
+            }
+        } else if (selected instanceof MuscleUI) {
+        }
+    }
+
     /**
      *
      */
@@ -314,7 +333,7 @@ public class MSAAction extends AbstractAction implements IMSAUIService {
         if (asList == null || asList.isEmpty()) {
             StringSet dupNames = msaList.getDuplicateEntryNames();
             if (!dupNames.isEmpty()) {
-                final String msg = String.format("<html>The following sequence%s %s duplicate in both alignments:<br/>%s<br/>Please rename or remove the duplicate sequence%s</html>", dupNames.size() > 1? "s":"", dupNames.size() > 1? "are":"is", dupNames.toHtmlList(), dupNames.size() > 1? "s":"");
+                final String msg = String.format("<html>The following sequence%s %s duplicate in both alignments:<br/>%s<br/>Please rename or remove the duplicate sequence%s</html>", dupNames.size() > 1 ? "s" : "", dupNames.size() > 1 ? "are" : "is", dupNames.toHtmlList(), dupNames.size() > 1 ? "s" : "");
                 DialogDescriptor.Message m = new DialogDescriptor.Message(msg, DialogDescriptor.INFORMATION_MESSAGE);
                 m.setTitle("Can not perform profile alignment");
                 DialogDisplayer.getDefault().notify(m);
@@ -325,7 +344,7 @@ public class MSAAction extends AbstractAction implements IMSAUIService {
             StringList entryNames = msaList.getEntryNames();
             StringList dupNames = asNames.intersect(entryNames);
             if (!dupNames.isEmpty()) {
-                final String msg = String.format("<html>The following sequence%s %s duplicate:<br/>%s<br/>Please rename or remove the duplicate sequence%s</html>", dupNames.size() > 1 ? "s":"", dupNames.size() > 1 ? "are":"is", dupNames.toHtmList(), dupNames.size() > 1 ? "s":"");
+                final String msg = String.format("<html>The following sequence%s %s duplicate:<br/>%s<br/>Please rename or remove the duplicate sequence%s</html>", dupNames.size() > 1 ? "s" : "", dupNames.size() > 1 ? "are" : "is", dupNames.toHtmList(), dupNames.size() > 1 ? "s" : "");
                 DialogDescriptor.Message m = new DialogDescriptor.Message(msg, DialogDescriptor.INFORMATION_MESSAGE);
                 m.setTitle("Can not perform profile alignment");
                 DialogDisplayer.getDefault().notify(m);
